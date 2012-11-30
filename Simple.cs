9ee403b4bcde0ab0742d89deb112f9dda6257fc8
Brent Botta <bbotta@x1discovery.com>
2012-11-30 06:54:29 -0800

    Updated new writeline

--- Simple.cs
+++ Simple.cs
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 using System;
+=======
+>>>>>>> Updated new writeline
 
 class HelloWorld
 {
@@ -8,6 +11,11 @@ class HelloWorld
         Console.WriteLine("WE ARE IN THE DEBUG CONFIGURATION");
 #endif
 
+<<<<<<< HEAD
         Console.WriteLine("This is a test of the new Bamboo server for X1 Discovery");
     }
+=======
+        Console.WriteLine("Let this compile due to the trigger.");
+  }
+>>>>>>> Updated new writeline
 }
