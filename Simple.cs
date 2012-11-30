ba6afe08a08264666bba67a19100392c34dba61b
bbotta <bbotta@x1discovery.com>
2012-11-29 22:14:07 -0800

    Update Simple.cs

--- Simple.cs
+++ Simple.cs
@@ -1,21 +1,14 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1187\cocoasubrtf340
-{\fonttbl\f0\fnil\fcharset0 Consolas;}
-{\colortbl;\red255\green255\blue255;\red255\green255\blue255;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\sl420
+using System;
 
-\f0\fs28 \cf0 \cb2 using System;\
-\
-class HelloWorld\
-\{\
-    static void Main()\
-    \{\
-#if DebugConfig\
-        Console.WriteLine("WE ARE IN THE DEBUG CONFIGURATION");\
-#endif\
-\
-        Console.WriteLine("Hello, world!");\
-    \}\
-\}\
+class HelloWorld
+{
+    static void Main()
+    {
+#if DebugConfig
+        Console.WriteLine("WE ARE IN THE DEBUG CONFIGURATION");
+#endif
+
+        Console.WriteLine("Hello, world!");
+    }
+}
 }
